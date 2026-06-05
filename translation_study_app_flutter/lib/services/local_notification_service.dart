c481b119b2d4513b6d95486b4e34008790d75ced
dlr0w <sekai1998@icloud.com>
2026-06-06 01:04:37 +0900

    通知機能実装のための環境構築環境構築

---
+++ translation_study_app_flutter/lib/services/local_notification_service.dart
@@ -0,0 +1,7 @@
+class LocalNotificationService {  
+    // 通知プラグインの初期化
+    // Android/iOSの通知許可リクエスト
+    // タイムゾーン初期化
+    // 毎日20時通知の予約
+    // 通知タップ時のpayload受け取り
+}
--- translation_study_app_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ translation_study_app_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,9 @@ import Foundation
 
 import connectivity_plus
 import desktop_webview_window
+import flutter_local_notifications
 import flutter_secure_storage_darwin
+import flutter_timezone
 import flutter_web_auth_2
 import google_sign_in_ios
 import path_provider_foundation
@@ -18,7 +20,9 @@ import window_to_front
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   DesktopWebviewWindowPlugin.register(with: registry.registrar(forPlugin: "DesktopWebviewWindowPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
+  FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
   FlutterWebAuth2Plugin.register(with: registry.registrar(forPlugin: "FlutterWebAuth2Plugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
