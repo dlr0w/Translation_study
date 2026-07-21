cabc315b50000802fe95217a32a509eeb3fa612e
dlr0w <sekai1998@icloud.com>
2026-07-22 00:54:20 +0900

    環境の設定

--- translation_study_app_flutter/android/app/build.gradle.kts
+++ translation_study_app_flutter/android/app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.11.1" apply false
+    id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
